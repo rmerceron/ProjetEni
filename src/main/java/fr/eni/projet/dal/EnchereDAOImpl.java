49370b2b5d5f40192419a3dd42fbd0249698b73e
SteevenT <tillier.steeven36@gmail.com>
2021-05-04 17:09:28 +0200

    Requete DAO et fin de journée !

--- src/main/java/fr/eni/projet/dal/EnchereDAOImpl.java
+++ src/main/java/fr/eni/projet/dal/EnchereDAOImpl.java
@@ -9,6 +9,63 @@ import fr.eni.projet.bo.Retrait;
 import fr.eni.projet.bo.Utilisateur;
 
 public class EnchereDAOImpl implements EnchereDAO {
+	
+	//Requete Insert
+	
+	private static final String INSERT_USER = "insert into utilisateur(pseudo, nom, prenom, email, telephone, rue, " +
+					"code_postal, ville, mot_de_passe, credit, administrateur) values(?,?,?,?,?,?,?,?,?,?,?);";
+	
+	private static final String INSERT_RETRAIT = "INSERT INTO RETRAITS (no_article, rue, code_postal, ville) VALUES (?,?,?,?);";
+	
+	private static final String INSERT_ARTICLE = "INSERT INTO ARTICLES_VENDUS (nom_article, description, date_debut_encheres, " +
+					"date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie) VALUES (?,?,?,?,?,?,?,?);";
+	
+	private static final String INSERT_ENCHERE = "INSERT INTO ENCHERES (date_enchere, montant_enchere, no_article, no_utilisateur) " +
+					"VALUES (?,?,?,?);";
+	
+	//Requete Select
+	
+	private static final String SELECT_USER = "SELECT no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, "+
+					"ville, mot_de_passe, credit, administrateur FROM utilisateurs;";
+	
+	private static final String SELECT_CATEGORIE = "SELECT no_categorie, libelle FROM categories;";
+	
+	private static final String SELECT_ARTICLE = "SELECT no_article, nom_article, description, date_debut_encheres, "+
+					"date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie FROM articles_vendus;";
+	
+	private static final String SELECT_ENCHERE = "SELECT no_enchere, date_enchere, montant_enchere, no_article, no_utilisateur FROM encheres;";
+	
+	private static final String SELECT_RETRAIT = "SELECT no_article, rue, code_postal, ville FROM retraits;";
+	
+	//Requete Update
+	
+	private static final String UPDATE_USER = "UPDATE UTILISATEURS SET pseudo = ?, nom = ?, prenom = ?, email = ?, telephone = ?, rue = ?, " +
+					"code_postal = ?, ville = ?, mot_de_passe = ?, credit = ?, administrateur = ? WHERE no_utilisateur = ?;";
+	
+	private static final String UPDATE_ENCHERE = "UPDATE ENCHERES SET date_enchere = ?, montant_enchere = ?, no_article = ?, no_utilisateur = ? " + 
+					"WHERE no_enchere = ?;";
+	
+	private static final String UPDATE_ARTICLE = "UPDATE ARTICLES_VENDUS SET nom_article = ?, description = ?, " +
+					"date_debut_encheres = ?, date_fin_encheres = ?, prix_initial = ?, prix_vente = ?, " +
+					"no_utilisateur = ?, no_categorie = ? WHERE no_article = ?;";
+	
+	private static final String UPDATE_RETRAIT = "UPDATE RETRAITS SET rue = ?, code_postal = ?, ville = ? WHERE no_article = ?;";
+	
+	//Requete Delete
+	
+	private static final String DELETE_USER = "DELETE FROM UTILISATEURS WHERE no_utilisateur = ?;";
+	
+	private static final String DELETE_ENCHERE = "DELETE FROM ENCHERES WHERE no_article = ?;";
+	
+	private static final String DELETE_ARTICLE = "DELETE FROM ARTICLES_VENDUS WHERE no_enchere = ?;";
+	
+	private static final String DELETE_RETRAIT = "DELETE FROM ARTICLES_VENDUS WHERE no_enchere = ?;";
+	
+	
+	
+	
+	
+	
 
 	@Override
 	public List<Utilisateur> selectUtilisateurs() {
